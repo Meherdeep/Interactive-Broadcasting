228f7b020bd668041942d3207bd64be053218612
Meherdeep <meherdeept@gmail.com>
2020-05-02 03:10:42 +0530

    first commit

---
+++ lib/pages/audience.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class AudienceWindow extends StatefulWidget {
+  @override
+  _AudienceWindowState createState() => _AudienceWindowState();
+}
+
+class _AudienceWindowState extends State<AudienceWindow> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Audience'),
+      ),
+      
+    );
+  }
+}
---
+++ lib/pages/broadcaster.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class BroadcasterWindow extends StatefulWidget {
+  @override
+  _BroadcasterWindowState createState() => _BroadcasterWindowState();
+}
+
+class _BroadcasterWindowState extends State<BroadcasterWindow> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Broadcaster'),
+      ),
+      
+    );
+  }
+}
